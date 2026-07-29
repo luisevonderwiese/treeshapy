85a5814a13e979d3f7690a61a167d1984b3fe0ae
Luise Haeuser <haeusele@hitssv566.villa-bosch.de>
2026-07-29 14:12:46 +0200

    improve example

--- example/example.py
+++ example/example.py
@@ -1,8 +1,8 @@
 from ete3 import Tree
 
-from treeshapy import treeshapy
-from treeshapy.treeshapy import TreeShape
-from treeshapy.all_root_treeshapy import AllRootTreeShape
+import treeshapy
+from treeshapy import TreeShape
+from treeshapy import AllRootTreeShape
 
 #list of all implemented indices
 print(treeshapy.INDICES)
@@ -16,22 +16,24 @@ ts = TreeShape(t, mode = "BINARY", rooted = True)
 
 #evaluate colless index (absolute)
 c = ts.absolute("colless_index")
+print(c)
 #evaluate colless index (normalized with number of tips)
-c = ts.relative("colless_index", "TIPS")
+c = ts.relative("colless_index", rel = "TIPS")
 #evaluate colless index (normalized with maximum value)
-c = ts.relative("colless_index", "MAX")
+c = ts.relative("colless_index", rel = "MAX")
 #evaluate colless index (normalized with expected value under the yule model)
-c = ts.relative("colless_index", "YULE")
+c = ts.relative("colless_index", rel = "YULE")
 
 #evaluate absolute values for all indices
 #returns a dict with indices as keys
 #if the index is not defined for the input tree, the respective entriy is nan
 res = ts.all_absolute()
+print(res["colless_index"])
 #evaluate relative values (normalized by number of tips) for all indices
 #also possible for the other normalization approaches
 #returns a dict with indices as keys
 #if the index is not defined for the input tree, the respective entriy is nan
-res = ts.all_relative("TIPS")
+res = ts.all_relative(rel = "TIPS")
 
 # evaluation for the 10 indices with minimum pairwise spearman rank correlation based on our experiments
 # possible for k = 2,..,10
@@ -47,13 +49,15 @@ ats = AllRootTreeShape(t, mode = "BINARY", rooted = True)
 #dictionary of the corresponding rooted topologies
 ats.all_rooted_trees
 #dictionary of the absolute colless index values
-ats.absolute("colless_index")
+res = ats.absolute("colless_index")
+print(res["internal_0"])
 #dictionary of the relative colless index values, normalized by the number of tips (analog for the other normalization options)
-ats.relative("colless_index", "TIPS")
+res = ats.relative("colless_index", "TIPS")
 #dictionary of all defined absolute index values
-ats.all_absolute()
+res = ats.all_absolute()
+print(res["internal_0"]["colless_index"])
 #dictionary of all defined relative index values, normalized by the number of tips (analog for the other normalization options)
-ats.all_relative("TIPS")
+res = ats.all_relative(rel = "TIPS")
 #dictionary of the absolute k index values with the minimum pairwise spearman rank correlation in our experiments
-ats.subset_absolute(k = 10)
+res = ats.subset_absolute(k = 10)
 
