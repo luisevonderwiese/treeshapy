59edb4bde6bdd5dc8ca23078ddcceff7ddb8580b
Luise Haeuser <haeusele@hitssv566.villa-bosch.de>
2026-09-02 12:46:13 +0200

    update subsets

--- treeshapy/subsets.py
+++ treeshapy/subsets.py
@@ -1,9 +1,9 @@
-INDEX_SUBSETS = {   2 : ['maximum_width', 'stairs1'],
-                    3 : ['bcent_root', 'root_imbalance', 'I_2_index'],
-                    4 : ['corrected_colless_index', 'average_ladder', 'I_root', 'stairs1'],
-                    5 : ['corrected_colless_index', 'maxdiff_widths', 'average_ladder', 'I_root', 'stairs1'],
-                    6 : ['B_2_index', 'maxdiff_widths', 'cherry_index', 'average_ladder', 'I_root', 'stairs1'],
-                    7 : ['variance_of_leaves_depths', 'corrected_colless_index', 'B_2_index', 'maxdiff_widths', 'average_ladder', 'I_root', 'stairs1'],
-                    8 : ['B_2_index', 'maxdiff_widths', 'modified_maxdiff_widths', 'cherry_index', 'average_ladder', 'I_root', 'stairs1', 'mean_I_prime'],
-                    9 : ['variance_of_leaves_depths', 'corrected_colless_index', 'B_2_index', 'maxdiff_widths', 'modified_maxdiff_widths', 'average_ladder', 'I_root', 'stairs1', 'I_2_index'],
-                    10 : ['B_1_index', 'B_2_index', 'maxdiff_widths', 'modified_maxdiff_widths', 'cherry_index', 'average_ladder', 'I_root', 'stairs1', 'mean_I_prime', 'mean_I_w']}
+INDEX_SUBSETS = {   2 : ['mean_I_prime', 'cherry_index'],
+                    3 : ['I_2_index', 'bcent_root', 'root_imbalance'],
+                    4 : ['corrected_colless_index', 'stairs1', 'average_ladder', 'I_root'],
+                    5 : ['corrected_colless_index', 'stairs1', 'maxdiff_widths', 'average_ladder', 'I_root'],
+                    6 : ['stairs1', 'B_2_index', 'maxdiff_widths', 'modified_maxdiff_widths', 'average_ladder', 'I_root'],
+                    7 : ['stairs1', 'variance_of_leaves_depths', 'B_2_index', 'maxdiff_widths', 'max_width_over_max_depth', 'average_ladder', 'I_root'],
+                    8 : ['stairs1', 'mean_I_prime', 'B_2_index', 'maxdiff_widths', 'modified_maxdiff_widths', 'average_ladder', 'cherry_index', 'I_root'],
+                    9 : ['corrected_colless_index', 'I_2_index', 'stairs1', 'variance_of_leaves_depths', 'B_2_index', 'maxdiff_widths', 'modified_maxdiff_widths', 'average_ladder', 'I_root'],
+                    10 : ['stairs1', 'mean_I_prime', 'mean_I_w', 'B_1_index', 'B_2_index', 'maxdiff_widths', 'modified_maxdiff_widths', 'average_ladder', 'cherry_index', 'I_root']}
